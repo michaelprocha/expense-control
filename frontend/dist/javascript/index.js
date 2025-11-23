3f7f907a26cca4c6b63779a57f3e2d65213cb692
michaelprocha <michaelrocha9722@gmail.com>
2025-11-23 19:46:01 -0300

    Add redirection and error handling all project

--- frontend/dist/javascript/index.js
+++ frontend/dist/javascript/index.js
@@ -6,23 +6,32 @@ async function login(login) {
 	try {
 		const response = await fetch("http://localhost:3000/login", {
 			method: "POST",
+			credentials: "include",
 			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify(login)
+			body: JSON.stringify(login),
 		});
-        const objResponse = await response.json();
-        console.log(objResponse);
-    
+
+		if (!response.ok) {
+			const e = await response.json();
+			console.log(e.message);
+			return;
+		}
+
+		const data = await response.json();
+		if (data.redirect) {
+			window.location.href = data.redirect;
+			return;
+		}
 	} catch (e) {
-        console.log(e);
-        console.log('falhou')
-    }
+		console.log("ERRO DE REDE:", e.message);
+	}
 }
 
 form.addEventListener("submit", async (e) => {
 	e.preventDefault();
-    const loginUser = {
-        email: `${inputEmail.value}`,
-        password: `${inputPassword.value}`
-    }
+	const loginUser = {
+		email: `${inputEmail.value}`,
+		password: `${inputPassword.value}`,
+	};
 	await login(loginUser);
 });
