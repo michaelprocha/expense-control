6f002c1733c4f1bf1fdb743107b8c01e2ab77276
michaelprocha <michaelrocha9722@gmail.com>
2025-11-23 19:45:07 -0300

    Add redirection and error handling

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,8 @@ import login from "./javascript/login.js";
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
 	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
--- backend/javascript/login.js
+++ backend/javascript/login.js
@@ -1,6 +1,6 @@
-import pkg from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import db from "../database/db.js";
-import argon2 from 'argon2';
+import argon2 from "argon2";
 
 export default function login(req, res) {
 	let body = "";
@@ -10,7 +10,7 @@ export default function login(req, res) {
 	req.on("end", () => {
 		const data = JSON.parse(body);
 		console.log(data);
-		const query = 'SELECT email, access FROM usuarios WHERE email = ?';
+		const query = "SELECT user_id, email, access FROM usuarios WHERE email = ?";
 		db.query(query, data.email, async (e, result) => {
 			if (e) {
 				console.log(`Erro na consulta: ${e.message}`);
@@ -19,16 +19,26 @@ export default function login(req, res) {
 			if (result[0]) {
 				const ok = await argon2.verify(result[0].access, data.password);
 				if (!ok) {
-					res.statusCode = 200;
-					res.statusMessage = `Feito login`;
+					res.statusCode = 401;
+					res.statusMessage = `Erro ao logar`;
 					res.setHeader("Content-type", "application/json; charset=utf-8");
-					res.end(JSON.stringify({status: "incorreto"}));
+					res.end(JSON.stringify({ message: "senha incorreta" }));
 					return;
 				}
+
+				const token = jwt.sign({ id: result[0].user_id }, "token", { expiresIn: "1h" });
+
 				res.statusCode = 200;
-				res.statusMessage = `Feito login`;
+				res.statusMessage = `login`;
 				res.setHeader("Content-type", "application/json; charset=utf-8");
-				res.end(JSON.stringify({status: "correto"}));
+				res.setHeader("Set-Cookie", `token=${token}; HttpOnly; SameSite=None; Path=/`);
+				res.end(JSON.stringify({ redirect: "products.html" }));
+			} else {
+				res.statusCode = 401;
+				res.statusMessage = `Erro ao logar`;
+				res.setHeader("Content-type", "application/json; charset=utf-8");
+				res.end(JSON.stringify({ message: "email incorreto" }));
+				return;
 			}
 		});
 	});
